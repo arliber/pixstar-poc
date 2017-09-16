ccc3fe479c6f69cbfbb1797707ff269a809867ef
Arik Liber <leonliber@gmail.com>
2017-09-16 16:31:18 +0300

    init

--- js/main.js
+++ js/main.js
@@ -22,17 +22,36 @@ document.querySelector('#pixstar-carousel-next').addEventListener('click', () =>
 var allSlides = document.querySelectorAll('.pixstar-slide');
 for (var i = 0; i < allSlides.length; i++) {
   allSlides[i].addEventListener('click', function (event) {
-    
     showSelection(event.target.getAttribute('data-id'), event.target.querySelector('img').getAttribute('src'))
   });
 }
 
+function toggleBodyScroll(enabled) {
+  document.body.style.overflow = enabled ? 'auto' : 'hidden';
+}
+
 function showSelection(id, src) {
-  document.querySelector('#pixstar-lightbox-selection img').setAttribute('src', src)
+  //document.querySelector('#pixstar-lightbox-selection img').setAttribute('src', src);
+  var elSelection = document.querySelector('#pixstar-lightbox-selection');
+  var backgrounds = [
+    ' linear-gradient(to right, rgba(0,0,0,0.25) 0%, rgba(255,255,255,0) 2%, rgba(255,255,255,0) 4%)',
+    'linear-gradient(135deg, rgba(31,31,31,1) 0%, rgba(255,255,255,0) 30%)',
+    'url(' + src + ')'
+  ];
+  elSelection.style.background = backgrounds.join(', ');
+  elSelection.style.backgroundSize = 'cover';
+  elSelection.style.backgroundRepeat = 'no-repeat';
+  elSelection.style.backgroundPosition = '50% 50%';
+  toggleBodyScroll(false);
   swal({
     buttons: false,
+    closeOnClickOutside: false,
+    closeOnEsc: false,
     content: document.querySelector('#pixstar-lightbox')
   })
 }
 
-document.querySelector('#pixstar-lightbox-close').addEventListener('click', function() { swal.close() });
+document.querySelector('#pixstar-lightbox-close').addEventListener('click', function() { 
+  toggleBodyScroll(true);
+  swal.close();
+});
